10820c923d27b65f404ceebbcdc62cb9eb544509
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-10-14 23:45:14 +0200

    Add authentication bug
    Adds authentication bug where error code 404 and error code 50 are returned.

---
+++ auth.py
@@ -0,0 +1,24 @@
+from threading import TIMEOUT_MAX
+import tweepy
+import config
+import signal
+
+def authenticateConfig():
+    # Authenticate to Twitter    
+    try: 
+        auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET_KEY)
+        auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+
+        api = tweepy.API(auth) # Setting these properties true will make the api to automatically wait for rate limits to replenish
+
+        print("Authentication successful" + " " + api.get_user()) # Prints the name of the authenticated user
+
+        return api # Return the api object to caller
+    except tweepy.HTTPException as e:
+        print( "Error: Authentication Failed -", str(e))
+        return None # Return None to caller (TODO: add error handling)
+    
+    
+    
+if __name__ == "__main__":
+    authenticateConfig()
--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,21 +1,15 @@
 import pandas as pd
 import tweepy
-import api_keys 
-
+import config 
+import auth
 # Authenticate to Twitter
 # This authentication approach isn't sustainable, temporarily solution for testing. 
 # Transfer to OAuth2 and configparser for production.
 
-def auth():
-    try: 
-        auth = tweepy.OAuthHandler(api_keys.CLIENT_KEY, api_keys.CLIENT_SECRET_KEY)
-        auth.set_access_token(api_keys.ACCESS_TOKEN, api_keys.ACCESS_TOKEN_SECRET)
-        api = tweepy.API(auth)
-    except:
-        print("Error: Authentication Failed")
         
 def main():
-    auth()    
+    auth.authenticateConfig()
+        
 
 # If this script file's name is main, run the main function.
 if __name__ == "__main__": 
