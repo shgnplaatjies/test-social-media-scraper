bdaa6450bbb4670d1673c6a2c5e38199f32335fc
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-10-15 00:03:06 +0200

    Add error logging
    Added error logging to log errors in a neater way than printing to the console.

--- auth.py
+++ auth.py
@@ -1,9 +1,14 @@
+from fileinput import filename
 from threading import TIMEOUT_MAX
 import tweepy
 import config
 import signal
+import logging
+
 
 def authenticateConfig():
+    logging.basicConfig(filename='log/auth.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
     # Authenticate to Twitter    
     try: 
         auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET_KEY)
@@ -11,11 +16,11 @@ def authenticateConfig():
 
         api = tweepy.API(auth) # Setting these properties true will make the api to automatically wait for rate limits to replenish
 
-        print("Authentication successful") # Prints the name of the authenticated user
+        logging.info("Authentication successful")
 
         return api # Return the api object to caller
     except tweepy.HTTPException as e:
-        print( "Error: Authentication Failed -", str(e))
+        logging.error("Authentication failed - %s", str(e))
         return None # Return None to caller (TODO: add error handling)
     
     
